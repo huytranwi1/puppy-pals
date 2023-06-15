eadb2a166fe56d9f2ccf782face45805b875e31d
huytranwi1 <huytranwii12@gmail.com>
2023-06-14 18:23:48 -0700

    added new data, and console to the web

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import './App.css'
+import { puppyList } from './data.js'
+import { useState } from 'react';
+
 
 function App() {
 
+console.log("puppiesList: ", puppyList);
+
+const [puppies, setpuppies] = useState(puppyList)
+
   return (
-    <div>
-      
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}> {puppy.name} </p>
+        })
+      }
     </div>
   )
 }
